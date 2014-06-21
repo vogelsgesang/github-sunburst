4d5700b89dfad998c6a6f09a91ea75e12e4f2968
Adrian Vogelsgesang <adrian.vogelsgesang@tum.de>
2014-06-21 06:03:21 +0200

    first visualization: bar partition

--- js/gh-sunburst.js
+++ js/gh-sunburst.js
@@ -44,7 +44,7 @@ angular.module("gh.sunburst", ['uri-templates'])
   function extractTree(ghTree) {
     function extractSubtree(children) {
       var children = _.groupBy(children, function(child) {return child.path.substr(0, child.path.indexOf("/"))});
-      var directChildren = children[""];
+      var directChildren = children[""] !== undefined ? children[""] : [];
       delete children[""];
       children = _.map(children, function(subtree, path) {
         var children = _.map(subtree, function(c) {
@@ -52,7 +52,7 @@ angular.module("gh.sunburst", ['uri-templates'])
           return c;
         });
         return {
-          name: path,
+          path: path,
           children: extractSubtree(children)
         };
       });
@@ -60,6 +60,69 @@ angular.module("gh.sunburst", ['uri-templates'])
       return children;
     };
     var blobs = _.where(ghTree, {type: "blob"});
-    return extractSubtree(blobs);
+    var tree = extractSubtree(blobs);
+    return {
+      path: "/",
+      children: tree
+    };
+  }
+})
+.directive("sunburst", function($window) {
+  return {
+    restrict: "E",
+    scope: {
+      hierarchy: "=",
+    },
+    link: function(scope, element, attrs) {
+      //listen for resize events
+      $window.addEventListener("resize", function() {
+        scope.width = element[0].offsetWidth;
+        scope.$apply();
+      });
+      //read the initial width
+      scope.width = element[0].offsetWidth;
+
+      //we need to keep a copy of the tree since d3.layout.partion modifies the
+      //the tree (adds some properties) and I do not want to expose these
+      //changes to the user of this directive.
+      var hierarchyCopy = _.cloneDeep(scope.hierarchy);
+      //watch the variables and redraw chart if necessary
+      scope.$watch("hierarchy", function(newHierarchy) {
+        hierarchyCopy = _.cloneDeep(scope.hierarchy);
+        redraw();
+      });
+
+      //create the d3 instances which are reused
+      var colorScale = d3.scale.category20b();
+      var partition = d3.layout.partition()
+        .sort(null)
+        .size([1, 1])
+        .value(function(d) { return 1; });
+      //create the SVG skeleton
+      var svg = d3.select(element[0])
+        .append("svg")
+          .style("width", "100%");
+      var mainGroup = svg.append("g");
+      
+      function redraw() {
+        //adjust the basic layout
+        console.log("redraw");
+        var width = scope.width;
+        var height = width;
+        partition.size([width, height]);
+        svg.style("height", height+"px");
+        //mainGroup.attr("transform", "translate(" + width/2 + "," + height/2 + ")");
+        var path = mainGroup.datum(hierarchyCopy).selectAll("path")
+            .data(partition.nodes)
+          .enter().append("rect")
+            .attr("x", _.property("x"))
+            .attr("y", _.property("y"))
+            .attr("width", _.property("dx"))
+            .attr("height", _.property("dy"))
+            .style("fill", function(d) {return colorScale(d.path)})
+            .attr("title", _.property("path"));
+      }
+
+    }
   }
 });
